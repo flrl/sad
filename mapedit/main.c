8cace301eb2c4f6474ebc4bb17667f28b0f24185
flrl <flrl@users.noreply.github.com>
2016-07-06 19:43:11 +1000

    definitely need a map editor

---
+++ mapedit/main.c
@@ -0,0 +1,58 @@
+#include <config.h>
+
+#include <stdio.h>
+
+#include <SDL.h>
+
+int main(int argc __attribute__((unused)),
+         char **argv __attribute__((unused)))
+{
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    int shutdown = 0;
+
+    const uint32_t window_flags = 0;
+    window = SDL_CreateWindow("hello world",
+                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              400, 300,
+                              window_flags);
+
+    const uint32_t renderer_flags = SDL_RENDERER_ACCELERATED
+                                  | SDL_RENDERER_PRESENTVSYNC;
+    renderer = SDL_CreateRenderer(window, -1, renderer_flags);
+
+    SDL_Point rect_points[] = {
+        { 100, 100 },
+        { 300, 100 },
+        { 300, 200 },
+        { 100, 200 },
+        { 100, 100 },
+    };
+    int rect_points_count = 5;
+
+    while (!shutdown) {
+        SDL_Event e;
+
+        if (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    shutdown = 1;
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderDrawLines(renderer, rect_points, rect_points_count);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
