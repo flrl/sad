c04d432b8b78d0f407101b04d070918d7e7cdcdf
flrl <flrl@users.noreply.github.com>
2016-07-25 21:36:52 +1000

    mapedit: dedup camera update recalcs

--- mapedit/camera.c
+++ mapedit/camera.c
@@ -9,17 +9,14 @@ const float camera_unitpx = 128;
 static SDL_Point camera_offset = { 0, 0 };
 static SDL_Renderer *camera_renderer = NULL;
 
+static void camera_update_offset(void);
+
 void camera_init(SDL_Renderer *renderer)
 {
-    SDL_Rect viewport;
-
     camera_centre.x = camera_centre.y = 0;
     camera_renderer = renderer;
 
-    SDL_RenderGetViewport(camera_renderer, &viewport);
-
-    camera_offset.x = lroundf(camera_unitpx * camera_centre.x) - (viewport.w / 2);
-    camera_offset.y = lroundf(camera_unitpx * camera_centre.y) - (viewport.h / 2);
+    camera_update_offset();
 }
 
 void camera_destroy(void)
@@ -27,6 +24,18 @@ void camera_destroy(void)
     camera_offset.x = camera_offset.y = 0;
     camera_centre.x = camera_centre.y = 0;
     camera_renderer = NULL;
+}
+
+static void camera_update_offset(void)
+{
+    SDL_Rect viewport;
+
+    if (!camera_renderer) return;
+
+    SDL_RenderGetViewport(camera_renderer, &viewport);
+
+    camera_offset.x = lroundf(camera_unitpx * camera_centre.x) - (viewport.w / 2);
+    camera_offset.y = lroundf(camera_unitpx * camera_centre.y) - (viewport.h / 2);
 }
 
 SDL_Point to_screen(fpoint p)
@@ -47,18 +56,13 @@ fpoint from_screen(SDL_Point screen)
 
 static void camera_move(int x, int y, unsigned flipped)
 {
-    SDL_Rect viewport;
-
     if (!camera_renderer) return;
 
     if (flipped) { x = -x; y = -y; }
     camera_centre.x -= (x / camera_unitpx);
     camera_centre.y += (y / camera_unitpx);
 
-    SDL_RenderGetViewport(camera_renderer, &viewport);
-
-    camera_offset.x = lroundf(camera_unitpx * camera_centre.x) - (viewport.w / 2);
-    camera_offset.y = lroundf(camera_unitpx * camera_centre.y) - (viewport.h / 2);
+    camera_update_offset();
 }
 
 int camera_handle_event(const SDL_Event *e)
@@ -66,8 +70,7 @@ int camera_handle_event(const SDL_Event *e)
     switch (e->type) {
         case SDL_WINDOWEVENT:
             if (e->window.event != SDL_WINDOWEVENT_SIZE_CHANGED) break;
-            camera_offset.x = lroundf(camera_unitpx * camera_centre.x) - (e->window.data1 / 2);
-            camera_offset.y = lroundf(camera_unitpx * camera_centre.y) - (e->window.data2 / 2);
+            camera_update_offset();
             break;
         case SDL_MOUSEWHEEL:
             camera_move(e->wheel.x, e->wheel.y, e->wheel.direction);
